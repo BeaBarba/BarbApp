3dfdfd3c828a0eabfacf2b076bba80956b5de185
Beatrice Barba <beatrice.barba.02@outlook.it>
2025-12-17 12:56:01 +0100

    Created a new activity: DayCalendarActivity #27

---
+++ app/src/main/java/com/example/myapplication/ui/screen/DayCalendarActivity.kt
@@ -0,0 +1,102 @@
+package com.example.myapplication.ui.screen
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+import com.example.myapplication.ui.component.AddButton
+import com.example.myapplication.ui.component.BackButton
+import com.example.myapplication.ui.component.DatePickerModal
+import com.example.myapplication.ui.component.LargeCard
+import com.example.myapplication.ui.component.TopAppBar
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
+class DayCalendarActivity : ComponentActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent{
+            MyApplicationTheme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TopAppBar(
+                            id = "Giorno Selezionato",
+                            navigationIcon = { BackButton {  } },
+                            trailingIcon = {
+                                var selectedDate by remember { mutableStateOf<Long?>(null) }
+                                val openPicker = remember { mutableStateOf(false) }
+                                val datePickerState = rememberDatePickerState()
+
+                                IconButton(
+                                    onClick = { openPicker.value = true },
+                                    colors = IconButtonDefaults.iconButtonColors(
+                                        contentColor = MaterialTheme.colorScheme.onPrimary
+                                    )
+                                ) {
+                                    Icon(Icons.Default.DateRange, contentDescription = "Apri date picker")
+                                }
+
+                                DatePickerModal(
+                                    datePickerState = datePickerState,
+                                    openDialogState = openPicker,
+                                    onDateSelected = { millis ->
+                                        selectedDate = millis
+                                    }
+                                )
+                            }
+                        )
+                    },
+                    floatingActionButton = { AddButton {  } }
+                ) { contentPadding ->
+                    LazyColumn(
+                        modifier = Modifier.padding(
+                            top = contentPadding.calculateTopPadding() + 8.dp,
+                            start = contentPadding.calculateStartPadding(LayoutDirection.Ltr) + 8.dp,
+                            end = contentPadding.calculateEndPadding(LayoutDirection.Ltr) + 8.dp,
+                            bottom = contentPadding.calculateBottomPadding()
+                        )
+                    ) {
+                        items(appuntamenti){item ->
+                            LargeCard(
+                                type = item.tipo,
+                                title = item.indirizzo,
+                                subtitle = item.cliente,
+                                description = item.descrizione,
+                                items = materialList,
+                                onClick = {},
+                            )
+                            Spacer(Modifier.size(8.dp))
+                        }
+                        item{Spacer(Modifier.size(8.dp))}
+                        item{Spacer(Modifier.size(80.dp))}
+                    }
+
+                }
+            }
+        }
+    }
+}
